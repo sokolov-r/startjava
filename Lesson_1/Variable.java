8a0fe6a402f02ebf4ad7556fc15dfc1256349961
Sokolov Roman <sokolov.r.s@list.ru>
2020-01-16 15:20:52 +0300

    first homework

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+	public static void main(String[] args){
+		boolean isWinOS = true;
+		byte numCore = 2;
+		short memoryRam = 12288;
+		int hdd = 1024;
+		long procHz = 2800000000;
+		float procGHz = 2.8F;
+		double usbVer = 3.1;
+		char usbType = 'C';
+		System.out.println(isWinOS);
+		System.out.println(numCore);
+		System.out.println(memoryRam);
+		System.out.println(hdd);
+		System.out.println(procHz);
+		System.out.println(procGHz);
+		System.out.println(usbVer);
+		System.out.println(usbType);
+	}
+}
