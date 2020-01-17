a0a295111fb3f285ffa145edd985b56bfb1a3ddc
Sokolov Roman <sokolov.r.s@list.ru>
2020-01-17 16:17:25 +0300

    add Cycle

---
+++ Lesson_1/Cycle.java
@@ -0,0 +1,24 @@
+public class Cycle {
+	public static void main (String[] args) {
+
+		for (int i = 0; i < 21; i++) {
+			System.out.println(i);
+		}
+
+		int j = 6;
+		while (j > -7) {
+			System.out.println(j);
+			j = j - 2;
+		}
+
+		int num = 10;
+		int sum = 0;
+		do {
+			if ((num % 2) > 0) {
+				sum = sum + num;
+			}
+			num++;
+		} while (num < 20);
+		System.out.println (sum);
+	}
+}
