c5c0e189045e4d028ea5b320ee44e83b1167b275
Sokolov Roman <sokolov.r.s@list.ru>
2020-01-23 12:18:14 +0300

    add Person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+
+	boolean man = true;
+	String name = "Bob";
+	int height = 178;
+	int weight = 80;
+	int age = 20;
+
+	void move() {
+		System.out.println("move");
+	}
+	void  sit() {
+		System.out.println("sit");
+	}
+	void run() {
+		System.out.println("run");
+	}
+	void learnJava() {
+		System.out.println("learn Java");
+	}
+}
