cc39a6d00c6fc87927dbb50198c00d070cb67d5d
Sokolov Roman <sokolov.r.s@list.ru>
2020-01-24 15:27:08 +0300

    correct CalculatorTest

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class CalculatorTest {
 
 	public static void main(String[] args) {
 		Calculator calculatorOne = new Calculator();
+		Scanner scan = new Scanner(System.in);
+		byte flag = 0;
+		String answer;
 
-		calculatorOne.inputA();
-		calculatorOne.inputOperation();
-		calculatorOne.inputB();
-		calculatorOne.output();
+		while (true) {
+			calculatorOne.inputA();
+			calculatorOne.inputOperation();
+			calculatorOne.inputB();
+			calculatorOne.output();
+			while (true) {
+				System.out.println("Хотите продолжить? [да/нет]: ");
+				answer = scan.nextLine(); 
+				if (answer.equals("да")) {
+					break;
+				} else if (answer.equals("нет")) {
+					flag = 1;
+					break;
+				}
+			}
+			if (flag == 1) {
+				break;
+			}
+		}
 	}
 }
