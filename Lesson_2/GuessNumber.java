d4c3d13d10e66756c7bd9bae7a0ee2de0a2a9993
Sokolov Roman <sokolov.r.s@list.ru>
2020-01-31 11:55:07 +0300

    correct GuessNumber, GuessNumberTest

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -1,12 +1,59 @@
+import java.util.Scanner;
+
 public class GuessNumber {
 
 	private int randomNumber;
 
-	public int getRandomNumber() {
-		return randomNumber;
-	}
-
 	public void makeNumber() {
 		randomNumber = (int)(Math.random() * 100);
+	}
+
+	public void game(Player player1, Player player2) {
+		Scanner scan = new Scanner(System.in);
+		boolean isPlayer1 = false;
+		String answer = "";
+
+		while (!(answer.equals("нет"))) {
+			isPlayer1 = false;
+			makeNumber();
+			System.out.println("Отгадайте число от 0 до 100");
+
+			do {
+				if (isPlayer1 == false) {
+					System.out.println("Отвечает " + player1.getName());
+					player1.setNumber(scan.nextInt());
+					isPlayer1 = true;
+					if (player1.getNumber() > randomNumber) {
+						System.out.println("Загаданное число меньше");
+					} else if (player1.getNumber() < randomNumber) {
+						System.out.println("Загаданное число больше");
+					} else {
+						break;
+					}
+				} else {
+					System.out.println("Отвечает " + player2.getName());
+					player2.setNumber(scan.nextInt());
+					isPlayer1 = false;
+					if (player2.getNumber() > randomNumber) {
+						System.out.println("Загаданное число меньше");
+					} else if (player2.getNumber() < randomNumber) {
+						System.out.println("Загаданное число больше");
+					} else {
+						break;
+					}
+				}
+			} while ((player1.getNumber() != randomNumber) || (player2.getNumber() != randomNumber));
+			
+			if 	(player1.getNumber() == randomNumber) {
+				System.out.println("Выиграл " + player1.getName());
+			} else {
+				System.out.println("Выиграл " + player2.getName());
+			}
+
+			do {
+				System.out.println("Хотите продолжить? [да/нет]: ");
+				answer = scan.next();
+			} while (!(answer.equals("да") || answer.equals("нет")));
+		}
 	}
 }
--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -3,11 +3,8 @@ import java.util.Scanner;
 public class GuessNumberTest {
 
 	public static void main(String[] args) {
-
 		GuessNumber guessNumber = new GuessNumber();
 		Scanner scan = new Scanner(System.in);
-		boolean isPlayer1 = false;
-		String answer = "";
 
 		System.out.println("Введите имя первого игрока:");
 
@@ -17,47 +14,6 @@ public class GuessNumberTest {
 
 		Player player2 = new Player(scan.next());
 		
-		while (!(answer.equals("нет"))) {
-			isPlayer1 = false;
-			guessNumber.makeNumber();
-			System.out.println("Отгадайте число от 0 до 100");
-
-			do {
-				if (isPlayer1 == false) {
-					System.out.println("Отвечает " + player1.getName());
-					player1.setNumber(scan.nextInt());
-					isPlayer1 = true;
-					if (player1.getNumber() > guessNumber.getRandomNumber()) {
-						System.out.println("Загаданное число меньше");
-					} else if (player1.getNumber() < guessNumber.getRandomNumber()) {
-						System.out.println("Загаданное число больше");
-					} else {
-						break;
-					}
-				} else {
-					System.out.println("Отвечает " + player2.getName());
-					player2.setNumber(scan.nextInt());
-					isPlayer1 = false;
-					if (player2.getNumber() > guessNumber.getRandomNumber()) {
-						System.out.println("Загаданное число меньше");
-					} else if (player2.getNumber() < guessNumber.getRandomNumber()) {
-						System.out.println("Загаданное число больше");
-					} else {
-						break;
-					}
-				}
-			} while ((player1.getNumber() != guessNumber.getRandomNumber()) || (player2.getNumber() != guessNumber.getRandomNumber()));
-			
-			if 	(player1.getNumber() == guessNumber.getRandomNumber()) {
-				System.out.println("Выиграл " + player1.getName());
-			} else {
-				System.out.println("Выиграл " + player2.getName());
-			}
-
-			do {
-				System.out.println("Хотите продолжить? [да/нет]: ");
-				answer = scan.next();
-			} while (!(answer.equals("да") || answer.equals("нет")));
-		}
+		guessNumber.game(player1, player2);
 	}
 }
