ae1cda335e34ec6394f0acbf8e1c798f4d4d49ba
Sokolov Roman <sokolov.r.s@list.ru>
2020-02-06 13:36:38 +0300

    correct CalculatorTest

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -7,23 +7,29 @@ public class CalculatorTest {
 		Scanner scan = new Scanner(System.in);
 		String answer;
 		char operation;
+		boolean isOk;
 
 		do {
 			System.out.print("Введите первое число: ");
 			calculatorOne.setA(scan.nextInt());
-			
+
 			System.out.print("Введите знак математической операции: ");
 			operation = scan.next().charAt(0);
 			if ((operation == '+') || (operation == '-') || (operation == '*') || 
 				(operation == '/') || (operation == '%') || (operation == '^')) {
 				calculatorOne.setOperation(operation);
+				isOk = true;
+			} else {
+				System.out.println("Неверная операция");
+				isOk = false;
+			}
+
+			if (isOk == true) {
 				System.out.print("Введите второе число: ");
 				calculatorOne.setB(scan.nextInt());
 				System.out.println(calculatorOne.calculate());
-			} else {
-				System.out.println("Неверная операция");
 			}
-
+			
 			do {
 				System.out.println("Хотите продолжить? [да/нет]: ");
 				answer = scan.next();
