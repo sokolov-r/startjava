f201354da672952a6dda1cdb101f08f653e8cfaa
Sokolov Roman <sokolov.r.s@list.ru>
2020-03-31 20:08:53 +0300

    correct Player,GuessNumber

--- src/com/startjava/lesson_4/game/GuessNumber.java
+++ src/com/startjava/lesson_4/game/GuessNumber.java
@@ -7,73 +7,61 @@ public class GuessNumber {
 
 	private int randomNumber;
 	private Player player1, player2;
+	private boolean isOk;
 
 	public GuessNumber(Player player1, Player player2) {
 		this.player1 = player1;
 		this.player2 = player2;
 	}
 
-	public void start() {
+	private void printAnswers(Player player) {
+		for (int num : player.getNumbers()) {
+			System.out.print(num + " ");
+		}
+		System.out.println();
+	}
+
+	private void answer(Player player) {
 		Scanner scan = new Scanner(System.in);
+		if (player.getNumberOfAnswer() < 10) {
+			System.out.println("Отвечает " + player.getName());
+			player.setNumber(scan.nextInt());
+			player.setNumberInArr(player.getNumber());
+			player.setNumberOfAnswer(player.getNumberOfAnswer() + 1);
+
+			if (player.getNumber() > randomNumber) {
+				System.out.println("Загаданное число меньше");
+			} else if (player.getNumber() < randomNumber) {
+				System.out.println("Загаданное число больше");
+			} else {
+				System.out.println("Игрок " + player.getName() + " угадал число " + randomNumber + " c " + player.getNumberOfAnswer() + " попытки");
+				isOk = true;
+			}
+		}
+		if (player.getNumberOfAnswer() == 10){
+			System.out.println("У " + player.getName() + " закончились попытки");
+		}
+	}
+
+	public void start() {
 		randomNumber = (int)(Math.random() * 101);
 		System.out.println("Отгадайте число от 0 до 100");
 		System.out.println("У вас 10 попыток");
 
-		Arrays.fill(player1.getNumbers(), 0, player1.getNumberOfAnswer(), 0);
-		Arrays.fill(player2.getNumbers(), 0, player2.getNumberOfAnswer(), 0);
+		player1.arrayCleaning();
+		player2.arrayCleaning();
+
 		player1.setNumberOfAnswer(0);
 		player2.setNumberOfAnswer(0);
-
+		isOk = false;
 
 		do {
-			if (player1.getNumberOfAnswer() < 10) {
-				System.out.println("Отвечает " + player1.getName());
-				player1.setNumber(scan.nextInt());
-				player1.setNumberInArr(player1.getNumber());
-				player1.setNumberOfAnswer(player1.getNumberOfAnswer() + 1);
+			answer(player1);
+			answer(player2);
 
-				if (player1.getNumber() > randomNumber) {
-					System.out.println("Загаданное число меньше");
-				} else if (player1.getNumber() < randomNumber) {
-					System.out.println("Загаданное число больше");
-				} else {
-					System.out.println("Игрок " + player1.getName() + " угадал число " + randomNumber + " c " + player1.getNumberOfAnswer() + " попытки");
-					break;
-				}
-			}
-			if (player1.getNumberOfAnswer() == 10){
-				System.out.println("У " + player1.getName() + " закончились попытки");
-			}
-
-
-			if (player2.getNumberOfAnswer() < 10) {
-				System.out.println("Отвечает " + player2.getName());
-				player2.setNumber(scan.nextInt());
-				player2.setNumberInArr(player2.getNumber());
-				player2.setNumberOfAnswer(player2.getNumberOfAnswer() + 1);
-
-				if (player2.getNumber() > randomNumber) {
-					System.out.println("Загаданное число меньше");
-				} else if (player2.getNumber() < randomNumber) {
-					System.out.println("Загаданное число больше");
-				} else {
-					System.out.println("Игрок " + player2.getName() + " угадал число " + randomNumber + " c " + player2.getNumberOfAnswer() + " попытки");
-					break;
-				}
-			}
-			if (player2.getNumberOfAnswer() == 10){
-				System.out.println("У " + player2.getName() + " закончились попытки");
-			}
-
-			if (player1.getNumberOfAnswer() == 10) {
-				for (int num : player1.getNumbers()) {
-					System.out.print(num + " ");
-				}
-				System.out.println();
-				for (int num : player2.getNumbers()) {
-					System.out.print(num + " ");
-				}
-				System.out.println();
+			if ((player1.getNumberOfAnswer() == 10) || (isOk)) {
+				printAnswers(player1);
+				printAnswers(player2);
 				break;
 			}
 		} while (true);
--- src/com/startjava/lesson_4/game/Player.java
+++ src/com/startjava/lesson_4/game/Player.java
@@ -1,30 +1,13 @@
 package com.startjava.lesson_4.game;
+
+import java.util.Arrays;
+
 public class Player {
 
 	private String name;
 	private int number;
 	private int numberOfAnswer;
 	private int[] numbers = new int[10];
-
-	public int getNumberOfAnswer() {
-		return numberOfAnswer;
-	}
-
-	public void setNumberOfAnswer(int numberOfAnswer) {
-		this.numberOfAnswer = numberOfAnswer;
-	}
-
-	public void setNumberInArr(int number) {
-		this.numbers[numberOfAnswer] = number;
-	}
-
-	public void setNumbers(int[] numbers) {
-		this.numbers = numbers;
-	}
-
-	public int[] getNumbers() {
-		return numbers;
-	}
 
 	public Player(String name) {
 		this.name = name;
@@ -34,13 +17,31 @@ public class Player {
 		return name;
 	}
 
-	public void setNumber(int number) {
-		this.number = number;
-	}
-
 	public int getNumber() {
 		return number;
 	}
 
+	public void setNumber(int number) {
+		this.number = number;
+	}
 
+	public int getNumberOfAnswer() {
+		return numberOfAnswer;
+	}
+
+	public void setNumberOfAnswer(int numberOfAnswer) {
+		this.numberOfAnswer = numberOfAnswer;
+	}
+
+	public int[] getNumbers() {
+		return Arrays.copyOf(numbers, numberOfAnswer);
+	}
+
+	public void setNumberInArr(int number) {
+		this.numbers[numberOfAnswer] = number;
+	}
+
+	public void arrayCleaning() {
+		Arrays.fill(numbers, 0, numberOfAnswer, 0);
+	}
 }
