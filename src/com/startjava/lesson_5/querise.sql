d82ff984c8704f12ce8842323c54cfd07452c0cf
Sokolov Roman <sokolov.r.s@list.ru>
2020-04-03 22:54:06 +0300

    lesson_5

---
+++ src/com/startjava/lesson_5/querise.sql
@@ -0,0 +1,10 @@
+SELECT * FROM jaegers;
+SELECT * FROM jaegers WHERE status ='ok';
+SELECT * FROM jaegers WHERE mark = '6' OR mark = '1';
+SELECT * FROM jaegers ORDER BY mark DESC;
+SELECT * FROM jaegers WHERE launch = (SELECT MIN(launch) FROM jaegers);
+SELECT * FROM jaegers WHERE kaijukill = (SELECT MIN(kaijukill) FROM jaegers);
+SELECT * FROM jaegers WHERE kaijukill = (SELECT MAX(kaijukill) FROM jaegers);
+SELECT AVG(weight) FROM jaegers;
+UPDATE Jaegers SET kaijukill = kaijukill + 1 WHERE status = 'ok';
+DELETE FROM jaegers WHERE status = 'destr';
