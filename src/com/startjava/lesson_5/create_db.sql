5483b0b803a0d856f603ac0f933a32d517ab4b4b
Sokolov Roman <sokolov.r.s@list.ru>
2020-04-04 12:18:38 +0300

    correct lesson_5

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,11 +1,11 @@
 CREATE TABLE Jaegers (
 	id          SERIAL PRIMARY KEY,
-	modelName	TEXT,
-	mark		CHAR(1),
-	height		INTEGER,
-	weight		INTEGER,
-	status		TEXT,
-	origin		TEXT,
-	launch		DATE,
-	kaijuKill 	INTEGER
+	modelName   TEXT,
+	mark        CHAR(1),
+	height      INTEGER,
+	weight      INTEGER,
+	status      TEXT,
+	origin      TEXT,
+	launch      DATE,
+	kaijuKill   INTEGER
 );
